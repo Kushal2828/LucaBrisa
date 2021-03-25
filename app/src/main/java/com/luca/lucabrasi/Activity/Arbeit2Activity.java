ee640e6e4fffb5f6cf7057d0986ceae176bd9536
kushal2828 <kushalprajapati888@gmail.com>
2021-03-25 19:49:39 +0530

    Commit Lucabrisa 25-03-2021

--- app/src/main/java/com/luca/lucabrasi/Activity/Arbeit2Activity.java
+++ app/src/main/java/com/luca/lucabrasi/Activity/Arbeit2Activity.java
@@ -2,10 +2,15 @@ package com.luca.lucabrasi.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.luca.lucabrasi.R;
 import com.luca.lucabrasi.Utils.BaseActivity;
+
+import java.util.concurrent.TimeUnit;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,15 +19,14 @@ import cn.iwgang.countdownview.CountdownView;
 public class Arbeit2Activity extends BaseActivity {
 
 
-
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arbeit2);
         ButterKnife.bind(this);
-        CountdownView mCvCountdownView = (CountdownView)findViewById(R.id.timer);
-        mCvCountdownView.start(25200000); // Millisecond
+
         setTitle("Arbeit", View.GONE);
 
     }
@@ -34,5 +38,41 @@ public class Arbeit2Activity extends BaseActivity {
                 startActivity(new Intent(this, Arbeit3Activity.class));
                 break;
         }
+    }
+
+    private void setTimer(final TextView tvHour, TextView tvminute, TextView tvSecond, final LinearLayout llSpecialOffer) {
+        if (handler != null) {
+            handler.removeCallbacks(null);
+
+        } else {
+            handler = new Handler();
+        }
+
+        final int delay = 1000; //milliseconds
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                //do something
+                handler.postDelayed(this, delay);
+                String timer = tvHour.getText().toString() + ":" + tvminute.getText().toString() + ":" + tvSecond.getText().toString();
+                long time = convertInMilisecond(timer) - 1000;
+                if (time == 0) {
+                    llSpecialOffer.setVisibility(View.GONE);
+                } else {
+                    tvHour.setText(TimeUnit.MILLISECONDS.toHours(time) + "");
+                    tvminute.setText((TimeUnit.MILLISECONDS.toMinutes(time) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(time))) + "");
+                    tvSecond.setText((TimeUnit.MILLISECONDS.toSeconds(time) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time))) + "");
+
+                }
+            }
+        }, delay);
+    }
+    private long convertInMilisecond(String time) {
+
+        String[] tokens = time.split(":");
+        int secondsToMs = Integer.parseInt(tokens[2]) * 1000;
+        int minutesToMs = Integer.parseInt(tokens[1]) * 60000;
+        int hoursToMs = Integer.parseInt(tokens[0]) * 3600000;
+        long total = secondsToMs + minutesToMs + hoursToMs;
+        return total;
     }
 }
